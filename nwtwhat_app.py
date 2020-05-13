5885272a55f700eb85378bd5f4d47cfa6e257fd8
edithturn <edithpuclla20@gmail.com>
2020-05-13 12:39:16 -0700

    question03 done

--- nwtwhat_app.py
+++ nwtwhat_app.py
@@ -2,7 +2,9 @@ from Question import Question
 
 question_prompt= [
 "If an Ethernet port on a router were assigned an IP address of 21.121.54.71/15, which host address would be able to communicate with it? \n (a) 166.121.177.233 \n (b) 95.199.103.46 \n (c) 21.121.241.69 \n (d) 244.28.220.100 \n (e) 198.58.153.142 \n (f) 79.46.141.190 \n (g) 167.232.175.162\n\n",
-"Which of the following services use UDP? 1. DHCP  2. SMTP  3. FTP  4. HTTP \n (a) 3 \n (b) 1 \n (c) All of the above \n (d) 2 \n\n"
+"Which of the following services use UDP? 1. DHCP  2. SMTP  3. FTP  4. HTTP \n (a) 3 \n (b) 1 \n (c) All of the above \n (d) 2 \n\n",
+"Which of the following is the valid host range for the subnet on which the IP address 120.2.67.128/15 resides \n (a) 10.2.0.1-120.3.255.251 \n (b) 120.2.0.2-120.3.255.254 \n (c) 120.2.0.1-120.3.255.253 \n (d) 120.2.0.1-120.4.0.3\n\n"
+
 ]
 
 questions = [
